70e71f9f6f61544467d5ea44d95002106a131420
Poonkodi <poonkodi@SenGs-MacBook-Pro.local>
2018-06-16 22:13:00 +0530

    fixes css and js file path issue.

--- resources/views/layouts/partials/backend/sidebar-menu.blade.php
+++ resources/views/layouts/partials/backend/sidebar-menu.blade.php
@@ -6,6 +6,11 @@
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
         </a>
     </li>
+    <li class="{{ \App\Utils::checkRoute(['dashboard::index', 'admin::index']) ? 'active': '' }}">
+        <a href="{{ route('dashboard::index') }}">
+            <i class="fa fa-dashboard"></i> <span>Category</span>
+        </a>
+    </li>
     @if (Auth::user()->can('viewList', \App\User::class))
         <li class="{{ \App\Utils::checkRoute(['admin::users.index', 'admin::users.create']) ? 'active': '' }}">
             <a href="{{ route('admin::users.index') }}">
