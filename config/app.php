85cb9737a4a54ec455b3ba58a858d9e4d5d42554
selvam <selvam@pearlthoughts.com>
2018-06-22 16:06:10 +0530

    Adds html form.

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -214,6 +215,8 @@ return [
         'Flash' => Laracasts\Flash\Flash::class,
         'Breadcrumbs' => DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::class,
         'Carbon' => Carbon\Carbon::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/admin/decks/form.blade.php
+++ resources/views/admin/decks/form.blade.php
@@ -1,10 +1,32 @@
-<div class="col-md-7">
-    <div class="col-md-12">
-        
-        <!-- /.form-group -->
+{!! Form::open(array('route' => 'admin::cards.create')) !!}
+<div class="col-md-12">
+    <div class="col-md-10">
+        <div class="form-group">
+        {{ Form::label('name', 'Name', ['class' => 'col-lg-2 control-label required']) }}
+        {{ Form::text('name', null, ['class' => 'form-control box-size', 'placeholder' => 'Name', 'required' => 'required']) }}
+        </div>
     </div>
-    <!-- /.col-md-12 -->
 
-    <!-- /.col-md-12 -->
+    <div class="col-md-10">
+        <div class="form-group">
+        {{ Form::label('categoryId', 'Category', ['class' => 'col-lg-2 control-label required']) }}
+        {{ Form::text('categoryId', null, ['class' => 'form-control box-size', 'placeholder' => 'Category', 'required' => 'required']) }}
+        </div>
+    </div>
 
+    <div class="col-md-10">
+        <div class="form-group">
+        {{ Form::label('status', 'Status', ['class' => 'col-lg-2 control-label required']) }}
+        {{ Form::select('status', array('L' => 'Large', 'S' => 'Small')) }}
+        </div>
+    </div>
+    
+    <div class="col-md-10">
+        <div class="form-group">
+        {{ Form::label('description', 'Description', ['class' => 'col-lg-2 control-label required']) }}
+        {{ Form::textarea('description', null, ['class' => 'form-control box-size', 'placeholder' => 'Description', 'required' => 'required']) }}
+        </div>
+    </div>
 </div>
+<div class="clearfix"></div>
+{!! Form::close() !!}
